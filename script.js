358aee22cd158cb6653aeabbb38c3b1fbd9ffa52
ayfabio <127374141+AyFabio@users.noreply.github.com>
2023-06-16 03:28:34 -0400

    Add button to change grid size.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector('.container');
+let gridContainer = document.querySelector('.grid-container');
 
 let row = document.createElement("div");
 row.classList.add("row");
@@ -6,12 +6,32 @@ row.classList.add("row");
 let column = document.createElement("div");
 column.classList.add("column");
 
-let gridSize = 16 ;
+let gridSize = 12;
+let btn = document.querySelector("button");
 
-for (let i = 0; i < gridSize; i++) { //make 16 columns of divs in a row
-    row.appendChild(column.cloneNode());
+btn.addEventListener("click", () => { //button changes grid size.
+    let newGridSize = prompt("Please choose a number between 1 - 100.")
+    if (newGridSize === null) return;
+    while (isNaN(newGridSize) || +newGridSize < 1 || +newGridSize > 100) {
+        newGridSize = prompt("Invalid choice! Please choose a number between 1 - 100. ")
+        if (newGridSize === null) return;
     }
+    changeGridSize(newGridSize);
+});
 
-for (let i = 0; i < gridSize; i++) { //make 16 rows of that div
-container.appendChild(row.cloneNode(true));
+
+function changeGridSize(gridSize) { //reset grid then remake grid
+    gridContainer.textContent = '';
+    row.textContent = '';
+    column.textContent = '';
+
+    for (let i = 0; i < gridSize; i++) { //make 16 columns of divs in a row
+        row.appendChild(column.cloneNode());
+        }
+
+    for (let i = 0; i < gridSize; i++) { //make 16 rows of that div
+    gridContainer.appendChild(row.cloneNode(true));
+    }   
 }
+
+changeGridSize(gridSize);
