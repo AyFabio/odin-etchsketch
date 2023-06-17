ad633b16f65c1ef087912f707557bfaeb07c5dd8
ayfabio <127374141+AyFabio@users.noreply.github.com>
2023-06-17 03:25:17 -0400

    Add draw on mouse down

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ row.classList.add("row");
 let column = document.createElement("div");
 column.classList.add("column");
 
-let gridSize = 30;
+let gridSize = 16;
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", () => { //button changes grid size.
     let newGridSize = prompt("Please choose a number between 1 - 100.")
-    if (newGridSize === null) return;
+    if (newGridSize === null) return; 
     while (isNaN(newGridSize) || +newGridSize < 1 || +newGridSize > 100) {
         newGridSize = prompt("Invalid choice! Please choose a number between 1 - 100. ")
         if (newGridSize === null) return;
@@ -32,6 +32,33 @@ function changeGridSize(gridSize) { //reset grid then remake grid
     for (let i = 0; i < gridSize; i++) { //make 16 rows of that div
     gridContainer.appendChild(row.cloneNode(true));
     }   
+    initiateDrawing();
 }
+
+
+
+function initiateDrawing() {
+    let isDrawing = false;
+
+    const square = document.querySelectorAll('.column');
+    
+    square.forEach((square) => {
+        square.addEventListener('mousedown', () => {
+            isDrawing = true;
+            square.classList.add('is-drawing');
+        });
+
+        square.addEventListener('mouseup', () => {
+            isDrawing = false;
+        });
+
+        square.addEventListener('mouseover', () => {
+            if (isDrawing === true)
+            square.classList.add('is-drawing');
+        });
+    });
+}
+
+
 
 changeGridSize(gridSize);
